3ab1daffac77fd7452b480cc75f8ad591b8cd7c0
AkhmadeevBulat <78420889+AkhmadeevBulat@users.noreply.github.com>
2024-06-13 03:43:50 +0300

    Delete env.py

--- env.py
+++
@@ -1,9 +0,0 @@
-class Volumes:
-    def __init__(self):
-        self.ssh_login = "login"
-        self.ssh_password = "password"
-
-        self.port = 22
-
-        self.user_containers_file = 'user_containers/user_containers.json'  # Пользовательские контейнеры
-        self.script_containers_base_file = 'script_containers/script_containers.json'  # Контейнеры, которые создает скрипт
